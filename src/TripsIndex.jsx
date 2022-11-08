dcbbad2e19ac1155aedfe251c739e5c00d1c6eaf
RobynSpaulding <robynm27@gmail.com>
2022-11-08 13:08:48 -0800

    adds route link from TripsIndex to appropriate TripsShow page

--- src/TripsIndex.jsx
+++ src/TripsIndex.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function TripsIndex(props) {
   return (
     <div id="trips-index">
       <h1>All Trips</h1>
       {props.trips.map((trip) => (
         <div key={trip.id}>
-          <p>{trip.title}</p>
+          {/* <p>{trip.title}</p> */}
+          <Link to={`/trips/${trip.id}`}>{trip.title}</Link>
         </div>
       ))}
     </div>

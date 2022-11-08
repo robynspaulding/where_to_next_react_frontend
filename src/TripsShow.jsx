a599982087cfe2bf1f4dce88b25c0d52bbc1b42e
RobynSpaulding <robynm27@gmail.com>
2022-11-08 11:14:22 -0800

    adds data to trip show modal, install react router dom

--- src/TripsShow.jsx
+++ src/TripsShow.jsx
@@ -3,6 +3,9 @@ export function TripsShow(props) {
     <div id="trip-show">
       <h1>Trip Info</h1>
       <p>{props.trip.title}</p>
+      <img src={props.trip.image_url} />
+      <p>{props.trip.start_time}</p>
+      <p>{props.trip.end_time}</p>
     </div>
   );
 }
